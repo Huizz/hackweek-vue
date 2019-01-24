b0bc871fdc8b3d77a5ab7eb78114a1ee4da2a25d
See Yihui <cuoguo@gmail.com>
2019-01-24 14:40:02 +0800

    Add dashboard page and jio component

---
+++ src/components/Jio.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="jio">
+    <div class="jio__title">{{jio.title}}</div>
+    <div class="jio__date">{{jio.date}}</div>
+    <b-btn class="btn btn-info jio__btn" @click="show=true">Me!</b-btn>
+
+    <b-modal v-model="show" id="joinJioModal" title="Add me to the jio">
+      <div class="modal-body">
+        <div class="input-group mb-3 flex-column">
+          <input
+            type="text"
+            class="form-control mb-2 w-100"
+            v-model="newPersonName"
+            placeholder="Name"
+            aria-label="Name"
+          >
+          <input
+            type="text"
+            class="form-control w-100"
+            placeholder="Telegram handler"
+            aria-label="Telegram-handler"
+          >
+        </div>
+      </div>
+      <div slot="modal-footer">
+        <button type="button" class="btn btn-secondary mr-1" @click="show=false">Close</button>
+        <button type="button" class="btn btn-primary" @click="handleAddPerson">Save changes</button>
+      </div>
+    </b-modal>
+    <div v-for="(person, index) in persons" :key="index">{{person}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Jio extends Vue {
+  @Prop() private jio!: object;
+  private data() {
+    return {
+      show: false,
+      persons: [],
+      newPersonName: '',
+      newPersonHandle: '',
+    };
+  }
+
+  private handleAddPerson() {
+    this.$data.persons.push({
+      name: this.$data.newPersonName,
+      handle: this.$data.newPersonHandle,
+    });
+    this.$data.show = false;
+  }
+}
+</script>
+
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <Jio v-bind:jio='jio'/>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Jio from '@/components/Jio.vue';
+
+@Component({
+  components: {
+    Jio,
+  },
+  data() {
+    return {
+      jio: {
+        title: 'This is a jio',
+        date: '2019-04-05',
+      },
+    };
+  },
+})
+export default class Dashboard extends Vue {}
+</script>
+
